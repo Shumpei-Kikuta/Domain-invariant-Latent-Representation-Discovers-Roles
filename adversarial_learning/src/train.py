2e0710c725c85c5a770cb6a64435744924b9e986
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-12-30 11:32:09 +0900

    del meanless

--- adversarial_learning/src/train.py
+++ adversarial_learning/src/train.py
@@ -46,12 +46,6 @@ def save_params():
 
 
 args = set_parser()
-epoch = args.epoch
-dr_rate = args.dr_rate
-lambda_ = args.lambda_
-weight_decay = args.wd
-task_lr = args.task_lr
-disc_lr = args.disc_lr
 
 # Read translated files
 train_features = np.load("dump/train_features.npy")
@@ -63,7 +57,7 @@ if args.is_test_label:
 if args.is_val_label:
     val_labels = np.load("dump/val_labels.npy")
 
-hparam_suffix = "e{}_dr{}_wd_{}_tlr{}_dlr{}_lamda{}".format(epoch, dr_rate, weight_decay, task_lr, disc_lr, lambda_)
+hparam_suffix = "e{}_dr{}_wd_{}_tlr{}_dlr{}_lamda{}".format(args.epoch, args.dr_rate, args.wd, args.task_lr, args.disc_lr, args.lambda_)
 
 tensor_test_features = torch.tensor(test_features, requires_grad=True).to(device)
 tensor_train_features = torch.tensor(train_features, requires_grad=True).to(device)
@@ -81,24 +75,24 @@ X = torch.arange(train_features.shape[0] + test_features.shape[0])
 X_train = X[test_features.shape[0]:].to(device)
 X_test = X[:test_features.shape[0]].to(device)
 
-role_model = RoleModel(init_features=merge_features, dr_rate=dr_rate, class_label_num=len(y_train.unique())).to(device)
-discriminator = Discriminator(dr_rate, EMB_SIZE=merge_features.shape[1]).to(device)
+role_model = RoleModel(init_features=merge_features, dr_rate=args.dr_rate, class_label_num=len(y_train.unique())).to(device)
+discriminator = Discriminator(args.dr_rate, EMB_SIZE=merge_features.shape[1]).to(device)
 
 # loss and optimizer
 disc_criterion = nn.BCELoss()
-disc_optimizer = torch.optim.Adam(discriminator.parameters(), lr=disc_lr,  weight_decay=weight_decay)
+disc_optimizer = torch.optim.Adam(discriminator.parameters(), lr=args.disc_lr,  weight_decay=args.wd)
 
 task_criterion = nn.CrossEntropyLoss() 
-task_optimizer = torch.optim.Adam(role_model.parameters(), lr=task_lr, weight_decay=weight_decay)
+task_optimizer = torch.optim.Adam(role_model.parameters(), lr=args.task_lr, weight_decay=args.wd)
 
-for e in range(epoch):
+for e in range(args.epoch):
     # discriminator optimization
     discriminator.train()
     
     detached_weight = role_model.embed.weight.detach()
     disc_outputs = discriminator(detached_weight)
     
-    disc_loss = lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
+    disc_loss = args.lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
     
     disc_optimizer.zero_grad()
     disc_loss.backward(retain_graph=True)
@@ -111,7 +105,7 @@ for e in range(epoch):
     move_weight = role_model.embed.weight
     disc_outputs = discriminator(move_weight)
 
-    task_loss = task_criterion(task_outputs, y_train.long()) -  lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
+    task_loss = task_criterion(task_outputs, y_train.long()) -  args.lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
     
     task_optimizer.zero_grad()
     task_loss.backward()
