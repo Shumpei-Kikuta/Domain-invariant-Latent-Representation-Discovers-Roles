0c0e80cd9cade7c17d07e8ae86f33800dc872ab0
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-12-30 11:48:27 +0900

    train func

--- adversarial_learning/src/train.py
+++ adversarial_learning/src/train.py
@@ -16,33 +16,77 @@ import torch.nn.functional as F
 
 
 _logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s"
-)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s")
 device = torch.device("cuda: 0" if torch.cuda.is_available() else "cpu")
 
 
 def set_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--is_test_label", action="store_true", help="True if test labels are available for calculating test accuracy")
-    parser.add_argument("--is_val_label", action="store_true", help="True if validation labels are available for calculating validation accuracy")
-    parser.add_argument("--param_dir", default=None, help="preserve features and predicted labels")
-    parser.add_argument("--epoch", default=3000, type=int, help="How many times models iterate")
-    parser.add_argument("--dr_rate", default=0.5, type=float, help="Drop out rate should be set from 0 to 1")
-    parser.add_argument("--lambda_", default=10, type=float, help="Balance coefficient")
-    parser.add_argument("--wd", default=1e-4, type=float, help="weight decay rate")
-    parser.add_argument("--task_lr", default=1e-4, type=float, help="learning rate for Role-Model")
-    parser.add_argument("--disc_lr", default=1e-4, type=float, help="learning rate for Discriminator")
+    parser.add_argument("--is_test_label", action="store_true", \
+                        help="True if test labels are available for calculating test accuracy")
+
+    parser.add_argument("--is_val_label", action="store_true", \
+                        help="True if validation labels are available for calculating validation accuracy")
+
+    parser.add_argument("--param_dir", default=None, \
+                        help="preserve features and predicted labels")
+
+    parser.add_argument("--epoch", default=3000, type=int, \
+                        help="How many times models iterate")
+
+    parser.add_argument("--dr_rate", default=0.5, type=float, \
+                        help="Drop out rate should be set from 0 to 1")
+
+    parser.add_argument("--lambda_", default=10, type=float, \
+                        help="Balance coefficient")
+
+    parser.add_argument("--wd", default=1e-4, type=float, \
+                        help="weight decay rate")
+
+    parser.add_argument("--task_lr", default=1e-4, type=float, \
+                        help="learning rate for Role-Model")
+
+    parser.add_argument("--disc_lr", default=1e-4, type=float, \
+                        help="learning rate for Discriminator")
+
     args = parser.parse_args()
     return args
 
 
-def save_params():
-    if not os.path.exists(args.param_dir):
-        os.mkdir(args.param_dir)
-    np.save(args.param_dir+"/train_feature_{}.npy".format(hparam_suffix), after_train_features)
-    np.save(args.param_dir+"/test_feature_{}.npy".format(hparam_suffix), after_test_features)
-    np.save(args.param_dir+"/test_outputs_{}.npy".format(hparam_suffix), test_outputs)
+def save_params(param_dir, hparam_suffix, after_train_features, after_test_features, test_outputs):
+    if not os.path.exists(param_dir):
+        os.mkdir(param_dir)
+    np.save(param_dir+"/train_feature_{}.npy".format(hparam_suffix), after_train_features)
+    np.save(param_dir+"/test_feature_{}.npy".format(hparam_suffix), after_test_features)
+    np.save(param_dir+"/test_outputs_{}.npy".format(hparam_suffix), test_outputs)
+
+
+def train(D, R, lambda_, disc_criterion, tensor_disc_labels, X_train, y_train):
+    # discriminator optimization
+    discriminator.train()
+    detached_weight = role_model.embed.weight.detach()
+    disc_outputs = discriminator(detached_weight)
+
+    disc_loss = lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
+
+    disc_optimizer.zero_grad()
+    disc_loss.backward(retain_graph=True)
+    disc_optimizer.step()
+
+    # role-model optimization
+    role_model.train()
+    task_outputs = role_model(X_train)
+
+    move_weight = role_model.embed.weight
+    disc_outputs = discriminator(move_weight)
+
+    task_loss = task_criterion(task_outputs, y_train.long()) \
+                - lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
+
+    task_optimizer.zero_grad()
+    task_loss.backward()
+    task_optimizer.step()
+
 
 def main():
     args = set_parser()
@@ -57,7 +101,8 @@ def main():
     if args.is_val_label:
         val_labels = np.load("dump/val_labels.npy")
 
-    hparam_suffix = "e{}_dr{}_wd_{}_tlr{}_dlr{}_lamda{}".format(args.epoch, args.dr_rate, args.wd, args.task_lr, args.disc_lr, args.lambda_)
+    hparam_suffix = "e{}_dr{}_wd_{}_tlr{}_dlr{}_lamda{}".format(\
+        args.epoch, args.dr_rate, args.wd, args.task_lr, args.disc_lr, args.lambda_)
 
     tensor_test_features = torch.tensor(test_features, requires_grad=True).to(device)
     tensor_train_features = torch.tensor(train_features, requires_grad=True).to(device)
@@ -87,29 +132,7 @@ def main():
 
     for e in range(args.epoch):
         # discriminator optimization
-        discriminator.train()
-        
-        detached_weight = role_model.embed.weight.detach()
-        disc_outputs = discriminator(detached_weight)
-        
-        disc_loss = args.lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
-        
-        disc_optimizer.zero_grad()
-        disc_loss.backward(retain_graph=True)
-        disc_optimizer.step()
-        
-        # task optimization
-        role_model.train()
-        task_outputs = role_model(X_train)
-
-        move_weight = role_model.embed.weight
-        disc_outputs = discriminator(move_weight)
-
-        task_loss = task_criterion(task_outputs, y_train.long()) -  args.lambda_ * disc_criterion(disc_outputs, tensor_disc_labels)
-        
-        task_optimizer.zero_grad()
-        task_loss.backward()
-        task_optimizer.step()
+        train(discriminator, role_model, args.lambda_, disc_criterion, tensor_disc_labels, X_train, y_train)
 
         role_model.eval()
         test_outputs = role_model(X_test)
@@ -132,12 +155,12 @@ def main():
     if args.is_val_label:
         _logger.info("val accuracy: {}".format(val_accuracy))
 
-
     if args.param_dir is not None:
         after_test_features = role_model.embed.weight.detach()[:test_features.shape[0], :].cpu().numpy()
         after_train_features = role_model.embed.weight.detach()[test_features.shape[0]:, :].cpu().numpy()
         test_outputs = test_outputs.detach().cpu().numpy()
         save_params(args.param_dir, hparam_suffix, after_train_features, after_test_features, test_outputs)
+
 
 if __name__ == '__main__':
     main()
